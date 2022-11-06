7004b7daf858ea00b59f48a0676f29639490c4ad
nurluyolerislam <nurluyolerislam@gmail.com>
2022-11-06 16:59:35 +0300

    Initial Commit

---
+++ KriptoMesaj/KriptoMesajApp.swift
@@ -0,0 +1,17 @@
+//
+//  KriptoMesajApp.swift
+//  KriptoMesaj
+//
+//  Created by Erislam Nurluyol on 6.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct KriptoMesajApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
